45407dea316d325ffd1a95e3d07368795b6b2fbd
tsmsogn <tsmsogn@gmail.com>
2019-03-12 00:14:30 +0900

    ./vendor/bin/phpunit tests/SecondTest.php

--- tests/SecondTest.php
+++ tests/SecondTest.php
@@ -2,12 +2,13 @@
 
 namespace Pear\Calendar\Test;
 
+use Pear\Calendar\Second;
 use PHPUnit_Framework_TestCase;
 
 class SecondTest extends PHPUnit_Framework_TestCase
 {
     function setUp() {
-        $this->cal = new Calendar_Second(2003,10,25,13,32,43);
+        $this->cal = new Second(2003,10,25,13,32,43);
     }
     function testPrevDay_Array () {
         $this->assertEquals(
